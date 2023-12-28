7b6f87eccd746d2bcd533cd52814725236b05f6c
snue <snue@proton.me>
2023-12-28 13:08:05 +0100

    rat now uses argp to parse flags, this broke some other stuff

--- main.c
+++ main.c
@@ -3,40 +3,61 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <argp.h>
+
 #include "math.h"
+
+const char *argp_program_version = "RAT 0.1";
+const char *argp_program_bug_address = "<snue@proton.me>";
+static char doc[] = "Rudimentary Arithmetic Tool designed for writing math in Kakoune.\nReads a statement from stdin and outputs the full equation, including the solution.";
+static char args_doc[] = "[MATH...]";
+
+static struct argp_option options[] = {
+    { "discard", 'd', 0, 0, "Discard input text."},
+};
+
+struct arguments
+{
+    //char *args[1];
+    int discard;
+};
+
+static error_t
+parse_opt (int key, char *arg, struct argp_state *state)
+{
+    struct arguments *arguments = state->input;
+
+    switch (key)
+    {
+        case 'd':
+            arguments->discard = 1;
+            break;
+        default:
+            return ARGP_ERR_UNKNOWN;
+    }
+    return 0;
+}
+
+static struct argp argp = { options, parse_opt, args_doc, doc };
 
 int main(int argc, char* argv[])
 {
-    for (int i = 1; i < argc; i++){
-        printf("%s ", argv[i]);
-    }
+    struct arguments arguments;
 
-    if (argc == 1){
-        char input[100];
-        fgets(input, 100, stdin);
-        for (int i = 0; input[i] != '\0'; i++){
-            if (input[i] == '\n'){
-                input[i] = '\0';
-            }
+    arguments.discard = 0;
+
+    argp_parse(&argp, argc, argv, 0, 0, &arguments);
+
+    char input[100];
+    fgets(input, 100, stdin);
+    for (int i = 0; input[i] != '\0'; i++){
+        if (input[i] == '\n'){
+            input[i] = '\0';
         }
-        printf("%s = %.4g\n", input, calc(input));
-        return 0;
     }
-
-    if (argc == 2){
-        printf("= %.4g\n", calc(argv[1]));
-        return 0;
-    }
-
-    switch (argv[1][0])
-    {
-        case 'g':
-            printf("= %d\n", gcd(str_to_int(argv[2]), str_to_int(argv[3])));
-            break;
-
-        default:
-            printf("= rat error\n");
-    }
-
+    
+    if (arguments.discard == 0)
+        printf("%s = ", input);
+    printf("%.4g\n", input, calc(input));
     return 0;
 }
